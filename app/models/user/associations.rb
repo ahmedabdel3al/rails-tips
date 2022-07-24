9d98a28bcd13b2c6806bea120e36ad60cfab9975
Ahmed <ahmed.abdelaal1921@mail.com>
2022-07-24 02:40:57 +0200

    has one of many in rails

--- app/models/user/associations.rb
+++ app/models/user/associations.rb
@@ -2,6 +2,8 @@ module User::Associations
   extend ActiveSupport::Concern
 
   included do
+      has_many :posts
+      has_one :last_post , ->{ joins('INNER JOIN ( SELECT MAX(id) AS id  FROM posts GROUP BY posts.user_id) AS latest_posts ON latest_posts.id = posts.id') } , class_name: Post.to_s
       has_many :user_logins
       has_one :last_login , -> () { joins('INNER JOIN ( SELECT MAX(id) AS id  FROM user_logins GROUP BY user_logins.user_id) AS latest_login ON latest_login.id = user_logins.id') }, class_name: UserLogin.to_s
 
